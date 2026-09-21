84d2a56083577812e00abe56f0fb0e84343694c5
Cathlyn Clyde Resmeros <cathlyn.jandog@gmail.com>
2026-09-21 15:39:33 +0800

    Add exercise 3.5 solution

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ module.exports = [
   },
   js.configs.recommended,
   {
-    files: ['app.js'],
+    files: ['*.js'],
     languageOptions: {
       ecmaVersion: 2018,
       sourceType: 'commonjs',
